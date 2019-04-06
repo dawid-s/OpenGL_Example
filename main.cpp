992bfe6365ba11e3208583d644d757a65ff9f03a
dawid-s <dawidskrodzki@gmail.com>
2019-04-06 16:35:09 +0100

    Doing some clang-tidy

--- main.cpp
+++ main.cpp
@@ -3,50 +3,52 @@
 
 #include <iostream>
 
-int main(void)
-{
-    GLFWwindow* window;
+class A {
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-    glewInit();
+};
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+int main() {
+        GLFWwindow *window;
+
+        /* Initialize the library */
+        if (glfwInit() == 0) {
+                return -1;
+        }
+        glewInit();
+
+        /* Create a windowed mode window and its OpenGL context */
+        window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+        if (window == nullptr) {
+                glfwTerminate();
+                return -1;
+        }
+
+        /* Make the window's context current */
+        glfwMakeContextCurrent(window);
+
+        if (glewInit() != GLEW_OK) {
+                std::cout << "Goew is not ok" << '\n';
+        }
+
+        /* Loop until the user closes the window */
+        while (glfwWindowShouldClose(window) == 0) {
+                /* Render here */
+                glClear(GL_COLOR_BUFFER_BIT);
+
+                glBegin(GL_TRIANGLES);
+                glColor3f(0.1f, 0.2f, 0.3f);
+                glVertex3f(0, 0, 0);
+                glVertex3f(1, 0, 0);
+                glVertex3f(0, 1, 0);
+                glEnd();
+
+                /* Swap front and back buffers */
+                glfwSwapBuffers(window);
+
+                /* Poll for and process events */
+                glfwPollEvents();
+        }
+
         glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    if (glewInit() != GLEW_OK){
-        std::cout << "Goew is not ok" << '\n';
-    }
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glBegin(GL_TRIANGLES);
-        glColor3f(0.1f, 0.2f, 0.3f);
-        glVertex3f(0, 0, 0);
-        glVertex3f(1, 0, 0);
-        glVertex3f(0, 1, 0);
-        glEnd();
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
+        return 0;
 }
